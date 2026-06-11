58b0021cc2930981802494e5473d14bd2bcfe34a
Yurii <kutsanyuri@gmail.com>
2026-06-11 19:36:33 +0300

    Switch

--- app.js
+++ app.js
@@ -1,11 +1,52 @@
-const secretNumber = '7';
+const role = 'admin';
 
-if(Number(secretNumber) === 7) {
-	console.log('Вгадав strong');
+if (role === 'manager') {
+	console.log('manager');
+} else if (role === 'admin') {
+	console.log('admin');
+} else if (role === 'ceo') {
+	console.log('ceo');
+} else {
+	console.log('Who are you?');
 }
 
-const q = Number(prompt('Enter sum'));
+switch (role) {
+	case 'manager':
+		console.log('manager');
+		break;
+	case 'admin': 
+	    console.log('admin');
+		break;
+	case 'ceo':
+		console.log('ceo');
+		break;
+	default:
+		console.log('Who are you?');
 
-if (q === 7) {
-	console.log('Right');
+}
+
+switch (role) {
+	case 'manager':
+	case 'admin': 
+	    console.log('no superviser');
+		break;
+	case 'ceo':
+		console.log('superviser');
+		break;
+	default:
+		console.log('Who are you?');
+
+}
+
+const num = 1;
+
+switch (true) {
+	case num > 0: 
+	console.log('positive');
+	break;
+	case num < 0:
+	console.log('negative');
+	break;
+	default:
+		console.log('zero');
 }
